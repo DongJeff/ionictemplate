5b2d25786d8b55a68d1695b3e15e0cd30fa5faab
DongJeff <jiefeidong@wustl.edu>
2023-02-28 10:09:48 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ionictemplate
+ 
---
+++ src/app/deals/details/deals-details.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IonicSwiper } from "@ionic/angular";
+
+import SwiperCore, { Pagination } from "swiper";
+
+SwiperCore.use([Pagination, IonicSwiper]);
+
+@Component({
+  selector: 'app-deals-details',
+  templateUrl: './deals-details.page.html',
+  styleUrls: [
+    './styles/deals-details.page.scss',
+    './styles/deals-details.shell.scss'
+  ]
+})
+export class DealsDetailsPage implements OnInit {
+
+  details: any;
+  slidesOptions: any = {
+    zoom: {
+      toggle: false // Disable zooming to prevent weird double tap zooming on slide images
+    }
+  };
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.data.subscribe(routeData => {
+      this.details = routeData['data'];
+    });
+  }
+}
